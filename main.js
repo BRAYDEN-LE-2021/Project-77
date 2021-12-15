23b3b695bf0f788c503aa24b2a3f4829cd9aafa3
BRAYDEN-LE-2021 <83260243+BRAYDEN-LE-2021@users.noreply.github.com>
2021-12-15 15:25:06 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name="Brayden";
+var last_name="Le";
+var a=8;
+var b=12;
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var add=a+b;
+console.log(add);
+var sub=a-b;
+console.log(sub);
+var mul=a*b;
+console.log(mul);
+var div=a/b;
+console.log(div);
